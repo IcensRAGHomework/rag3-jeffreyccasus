e3ad17fe4895954f5b3e3f837dbd0b47d79dc5f5
JeffreyCC <jeffrey_cc_chen@asus.com>
2025-03-17 14:52:39 +0800

    hw1

--- student_assignment.py
+++ student_assignment.py
@@ -18,6 +18,12 @@ from chromadb.utils import embedding_functions
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_core.documents import Document
 
+from chromadb.utils import embedding_functions
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_core.documents import Document
+import time
+import datetime
+
 def generate_hw01():
     # load csv
     csv_file_path = "COA_OpenData.csv"
@@ -30,7 +36,7 @@ def generate_hw01():
     csv_data_createdate = CSVLoader(file_path = csv_file_path, source_column="CreateDate").load()
     csv_data_hostwords = CSVLoader(file_path = csv_file_path, source_column="HostWords").load()
 
-    #print_csv_detail(csv_data)
+    #print(csv_data_name)
 
     # embedding function
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
@@ -38,37 +44,49 @@ def generate_hw01():
                     api_key = gpt_emb_config['api_key'],
                     api_type = gpt_emb_config['openai_type'],
                     api_version = gpt_emb_config['api_version'],
-                    model_name = gpt_emb_config['deployment_name'], 
+                    model_name = gpt_emb_config['deployment_name'],
                 )
-    
+
+
+    chroma_client = chromadb.PersistentClient(dbpath) # store in local machine
+
+    # deltet old one first
+    chroma_client.delete_collection(name="TRAVEL")
     # create chroma
-    chroma_client = chromadb.PersistentClient(dbpath)
     #collection = chroma_client.get_or_create_collection(
-    collection = chroma_client.get_or_create_collection(
+    collection = chroma_client.create_collection(
         name = "TRAVEL",
         metadata = {
+            #使用哪一種評估方式來了解後續詢問的問題，是否接近知識點，我們這邊選擇 Cosine Distance 來作為計算
             "hnsw:space": "cosine"
         },
         embedding_function = openai_ef
     )
 
-    if collection.count() !=0:
-        return collection
-
     # store csv data into collection from chroma
     size = len(csv_data_name)
     print(size)
+    # for deubg
     #for i in range(1): # start from 0
-        #print(csv_data_hostwords[i].metadata["source"])
-        #print(csv_data_name)
+    #    print(csv_data_name[i].metadata["source"])
+    #    print(csv_data_name[i])
+    #    print(csv_data_createdate[i].metadata["source"])
+    #    print(csv_data_createdate[i])
         #print("\n")
 
+    # convert data to list
     documents_metadatas = list()
     documents_hostwords = list()
     for i in range(size): # start from 0
         documents_hostwords.append(
             csv_data_hostwords[i].metadata["source"]
         )
+
+        create_date = csv_data_createdate[i].metadata["source"]
+        converted_seec = time.mktime(datetime.datetime.strptime(create_date, "%Y-%m-%d").timetuple())
+        if i == 0:
+          print("create_date =", create_date, ",is converted to sec = ", converted_seec)
+
         metadata_string = {
             "file_name" : csv_file_path,
             "name" : csv_data_name[i].metadata["source"],
@@ -77,12 +95,12 @@ def generate_hw01():
             "tel" : csv_data_tel[i].metadata["source"],
             "city" : csv_data_city[i].metadata["source"],
             "town" : csv_data_town[i].metadata["source"],
-            "date" : csv_data_createdate[i].metadata["source"],
+            "date" : converted_seec,
 
         }
         documents_metadatas.append(metadata_string)
 
-
+    # add to chroma collection
     collection.add(
         ids=[f"{i}" for i in range(size)],
         documents = documents_hostwords,
